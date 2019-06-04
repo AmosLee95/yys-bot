d2b558cbd18c84ccf44133b4d7233ba8f844bfd2
AmosLee95 <AmosLee94@126.com>
2019-06-04 21:14:29 +0800

    --

--- tools/config.py
+++
@@ -1,5 +0,0 @@
-supportList = [
-    ["LDPlayerMainFrame", "RenderWindow", "TheRender"],# 夜神
-    ["Qt5QWindowIcon","Qt5QWindowIcon", "ScreenBoardClassWindow"],# 夜神
-    ["Qt5QWindowIcon", "Qt5QWindowIcon", "NemuPlayer"]# mumu
-    ] 
