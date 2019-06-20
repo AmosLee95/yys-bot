9de14f0c575e1614e5ecbd609875b7a560545ffa
AmosLee95 <AmosLee94@126.com>
2019-06-20 23:57:15 +0800

    download and add again

--- main.py
+++ main.py
@@ -1,1 +1,82 @@
-import tools.dm
+from PyQt5 import QtWidgets 
+from PyQt5.QtCore import QTimer
+
+from qt.Ui_main import Ui_MainWindow 
+from qt.Ui_scriptTabs import Ui_tabFrame 
+from qt.Ui_script import Ui_ScriptControler 
+from qt.Ui_noWindow import Ui_NoWindow
+
+from tools.dm import Dm
+
+import time
+
+class MainWindow(QtWidgets.QMainWindow,Ui_MainWindow): 
+    def __init__(self, parent = None): 
+        super(MainWindow,self).__init__(parent) 
+        self.setupUi(self) 
+        self.dm = Dm()
+        self.windowInfos = self.dm.findWindows()
+        
+        if len(self.windowInfos) :
+            self.addScriptTabs(self.windowInfos)
+        else:
+            self.info=NoWindow()
+            self.gridLayout.addWidget(self.info)
+            self.info.show() 
+            
+
+    def addScriptTabs(self, windowInfos):
+        # 添加tab
+        self.scriptTabs=ScriptTabs(windowInfos)
+        self.gridLayout.addWidget(self.scriptTabs)
+        self.scriptTabs.show() 
+    
+class NoWindow(QtWidgets.QWidget,Ui_NoWindow): 
+    def __init__(self, parent = None): 
+        super(NoWindow,self).__init__(parent) 
+        self.setupUi(self) 
+  
+class ScriptTabs(QtWidgets.QWidget,Ui_tabFrame): 
+    def __init__(self,windowInfos, parent = None): 
+        super(ScriptTabs,self).__init__(parent) 
+        self.setupUi(self) 
+        self.tabs = []
+        for windowInfo in windowInfos:
+            self.addTab(self, windowInfo)
+    def addTab(self, ScriptTabs, windowInfo):
+        # add new tab
+        newTab = QtWidgets.QWidget()
+        newTab.setObjectName("tab_4")
+        self.tabWidget.addTab(newTab, windowInfo['title'])
+        self.tabs.append(newTab)
+        # add new scriptControler
+        gridLayout = QtWidgets.QGridLayout(newTab)
+        newScriptControler = ScriptControler(windowInfo)
+        gridLayout.addWidget(newScriptControler)
+        # newTab.
+
+class ScriptControler(QtWidgets.QWidget,Ui_ScriptControler): 
+    def __init__(self, windowInfo, parent = None): 
+        super(ScriptControler,self).__init__(parent) 
+        self.setupUi(self) 
+        self.windowInfo = windowInfo
+        self.dm = Dm(windowInfo['fatherHwnd'])
+        self.dm.childHwnd = windowInfo['childHwnd']
+        self.dm.fatherHwnd = windowInfo['fatherHwnd']
+        res = self.dm.tryBandWindow()
+        print('绑定成功' if res else '绑定失败！')
+        
+        self.timer = QTimer(self) #初始化一个定时器
+        self.timer.timeout.connect(self.timerFunction) #计时结束调用operate()方法
+        self.timer.start(2000) #设置计时间隔并启动
+    def timerFunction(self):
+        print('zzz...')
+        # time.sleep(1)
+        self.timer.stop()
+def run():
+    import sys 
+    app=QtWidgets.QApplication(sys.argv) 
+    mainWindow=MainWindow() 
+    mainWindow.show() 
+    sys.exit(app.exec_())
+run()
---
+++ qt/Ui_main.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'g:\python\yys-bot\qt\main.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(602, 447)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(0, 30, 601, 371))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(500, 0, 101, 16))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(10, 10, 54, 12))
+        self.label_2.setObjectName("label_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 602, 23))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "QQ群：567783492"))
+        self.label_2.setText(_translate("MainWindow", "yys-bot"))
+
